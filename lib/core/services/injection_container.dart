d0bdfa9043d6810d498f5198c11aa11a3c0bc91f
Martin <Martinhanyfcis@gmail.com>
2026-02-25 17:45:53 +0200

    Update injection_container.dart

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,4 +1,3 @@
-
 // Create a global instance of GetIt (Our Service Locator / Waiter)
 import 'package:get_it/get_it.dart';
 
@@ -7,31 +6,44 @@ import '../../features/home/data/repositories/home_repository_impl.dart';
 import '../../features/home/domain/repositories/home_repository.dart';
 import '../../features/home/domain/usecase/get_recent_files.dart';
 import '../../features/home/presentation/manager/home_bloc.dart';
+import 'network_service.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  // ==========================================
+  // Core
+  // ==========================================
+  sl.registerLazySingleton<NetworkService>(() => NetworkService());
+
   // ==========================================
   // Feature: Home
   // ==========================================
 
   // 1. Domain Layer: Use Cases
   // We register the UseCase and tell it to get the Repository from GetIt (sl())
+
+  //BloCs
   sl.registerFactory(() => HomeBloc(getRecentFilesUseCase: sl()));
+
+  //Use Cases
   sl.registerLazySingleton(() => GetRecentFilesUseCase(sl()));
 
   // 2. Data Layer: Repository
   // When the UseCase asks for 'HomeRepository' (Interface),
   // GetIt will give it 'HomeRepositoryImpl' (Implementation).
+
+  //Repositories
   sl.registerLazySingleton<HomeRepository>(
         () => HomeRepositoryImpl(sl()),
   );
 
   // 3. Data Layer: Data Sources
   // When the Repository asks for the remote data source, GetIt provides it.
+
+  //Data Sources
   sl.registerLazySingleton<HomeRemoteDataSource>(
         () => HomeRemoteDataSourceImpl(),
   );
 
-  // Note: We will add the HomeBloc here later once we create it in the Presentation layer!
 }
